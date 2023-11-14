5ca340be54cbc4d3b4c3638ab74e21f8e626c74f
Stanisław Kumor <stanislav.kumor@gmail.com>
2023-11-14 10:47:37 +0100

    Counter

---
+++ projects/counter/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from "./Components/Header";
+import Counter from "./Components/Counter";
+import './counter.css';
+
+function App() {
+    return (
+        <>
+
+            <div className="main">
+                <Header/>
+                <Counter/>
+            </div>
+
+
+        </>
+
+    );
+}
+
+export default App;
